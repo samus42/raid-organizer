996015e59f6a2971796a59662b6ae933492d1d4e
Scott MacDonald <samus42@yahoo.com>
2022-12-17 19:13:50 -0600

    updated lost sector rotation for season 19

--- src/rotations/lostSectors.js
+++ src/rotations/lostSectors.js
@@ -18,34 +18,37 @@ export const Rewards = {
     Arms: 'Arms'
 }
 
-export const FirstDayOfLostSectorRotation = dayjs('2022-10-28')
+export const FirstDayOfLostSectorRotation = dayjs('2022-12-06')
 
 const sectorRotation = [
     sector(`K1 Logistics`, Areas.Moon),
+    sector(`K1 Crew Quarters`, Areas.Moon),
+    sector(`K1 Revelation`, Areas.Moon),
     sector(`K1 Communion`, Areas.Moon),
+    sector('Bunker E15', Areas.Europa),
+    sector('Concealed Void', Areas.Europa),
+    sector('Perdition', Areas.Europa),
+    sector(`Sepulcher`, Areas.ThroneWorld),
+    sector(`Extraction`, Areas.ThroneWorld),
+    sector(`Chamber of Starlight`, Areas.DreamingCity),
+    sector(`Aphelion's Rest`, Areas.DreamingCity),
+]
+
+/*
+    sector('Veles Labyrinth', Areas.Cosmodrome),
+    sector('Exodus Garden 2A', Areas.Cosmodrome),
+    
+    sector(`Bay of Drowned Wishes`, Areas.DreamingCity),
+    sector(`Metamorphosis`, Areas.ThroneWorld),
     sector(`Skydock IV`, Areas.EDZ),
     sector(`Scavenger's Den`, Areas.EDZ),
     sector(`The Quarry`, Areas.EDZ),
     sector(`Excavation Site XII`, Areas.EDZ),
-    sector('Concealed Void', Areas.Europa),
-    sector('Bunker E15', Areas.Europa),
-    sector('Perdition', Areas.Europa),
     sector('The Conflux', Areas.Nessus),
-    // sector('The Rift', Areas.Nessus),
-    sector(`K1 Crew Quarters`, Areas.Moon),
-]
-
-/*
-    sector(`K1 Revelation`, Areas.Moon),
-
-    sector('Veles Labyrinth', Areas.Cosmodrome),
-    sector('Exodus Garden 2A', Areas.Cosmodrome),
-    sector(`Aphelion's Rest`, Areas.DreamingCity),
-    sector(`Bay of Drowned Wishes`, Areas.DreamingCity),
-    sector(`Chamber of Starlight`, Areas.DreamingCity),
-        sector(`Metamorphosis`, Areas.ThroneWorld),
-    sector(`Sepulcher`, Areas.ThroneWorld),
-    sector(`Extraction`, Areas.ThroneWorld),
+    sector('The Rift', Areas.Nessus),
+        
+    
+    
     
 
 */
