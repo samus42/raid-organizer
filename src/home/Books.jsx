942197f16a733239eeabf9c3464cfc4d98432cf6
Scott MacDonald <samus42@yahoo.com>
2022-12-28 12:01:15 -0500

    fixing books message

--- src/home/Books.jsx
+++ src/home/Books.jsx
@@ -4,8 +4,11 @@ import { Link, Avatar, Typography, Popover, Card, CardContent, List, ListItem } 
 
 const ace = 'Aaron Corcoran'
 const samus = 'Scott MacDonald'
+const tansy = 'Tracy Smith-Van Pelt'
 const samusIcon = 'https://www.bungie.net/img/profile/avatars/e2015_14.jpg'
 const aceIcon = 'https://www.bungie.net//img/profile/avatars/avatar23.jpg'
+const tansyIcon = 'https://image.api.np.km.playstation.net/images/?format=png&w=160&h=160&image=https%3A%2F%2Fkfscdn.api.np.km.playstation.net%2F5387964934898443052%2F1541722837287.png&sign=1cc7d0286c4fc59c83ec1f0c6a37f2d414df1daa'
+
 const dolArnach = {
     author: ace,
     icon: aceIcon,
@@ -31,7 +34,6 @@ const darkBalance = {
     author: samus,
     icon: samusIcon,
     title: 'A Dark Balance',
-    subtext: 'Just released!',
     pdf: 'https://drive.google.com/file/d/1gTzGi75dX5fK4Sr-p65teATbz4mpFuZ_/view?usp=sharing',
     epub: 'https://drive.google.com/file/d/19b-YBJl4gYCMleH9VM-hqDuTEsk-qkD4/view?usp=sharing'
 }
@@ -41,6 +43,13 @@ const redjackThief = {
     title: 'The Redjack Thief',
     pdf: 'https://drive.google.com/file/d/18fRu74TJTExGUI4KuevNN-OkAkOsp4IC/view?usp=sharing',
     epub: 'https://drive.google.com/file/d/1C98vmkwY-598Cm1OGHjrUpHkbsS7XoYP/view?usp=sharing'
+}
+const worstHunter = {
+    author: tansy,
+    icon: tansyIcon,
+    title: 'The Worst Hunter',
+    subtext: 'Just released!',
+    pdf: 'https://drive.google.com/file/d/12svAXAL4Y8UccfMK1iNVrfgqJxpHFaTQ/view?usp=share_link'
 }
 
 function BookItem({ book, onClick }) {
@@ -98,7 +107,7 @@ function BookDetails({ book }) {
                 <Typography variant="h6">Download Options</Typography>
                 <List>
                     <ListItem><a href={book.pdf} target="_blank" rel="noreferrer">PDF</a></ListItem>
-                    <ListItem><a href={book.epub} target="_blank" rel="noreferrer">ePub (for all eReaders, including Kindle)</a></ListItem>
+                    {book.epub && <ListItem><a href={book.epub} target="_blank" rel="noreferrer">ePub (for all eReaders, including Kindle)</a></ListItem>}
                 </List>
                 <Typography variant="h6">If you want a print copy, just ask!</Typography>
             </CardContent>
@@ -118,7 +127,7 @@ export default function Books() {
     }
     return (
         <div>
-            <Typography variant="h6">Aaron and Scott (Ace and Samus) have written a few Destiny novels using a shared universe. Give them a try! See the timeline below for reading order. Click on a book link to get download options.</Typography>
+            <Typography variant="h6">Our clan members have written a few Destiny novels using a shared universe. Give them a try! See the timeline below for reading order. Click on a book link to get download options.</Typography>
             <Timeline>
                 <EventItem event={`Crota's End`} />
                 <EventItem event={`Fall of Oryx`} />
@@ -127,6 +136,7 @@ export default function Books() {
                 <EventItem event="The Red War" />
                 <BookItem book={halilysWar} onClick={onBookClick} />
                 <BookItem book={darkBalance} onClick={onBookClick} />
+                <BookItem book={worstHunter} onClick={onBookClick} />
                 <EventItem event="Forsaken" />
                 <BookItem book={redjackThief} onClick={onBookClick} />
             </Timeline>
